977a61f796ef76f04e761e5febe634f7dad9eb76
Gustavo <gustavovalenca15@gmail.com>
2023-10-13 13:41:34 -0300

    project started

---
+++ minigrep/src/main.rs
@@ -0,0 +1,37 @@
+use std::env;  // panics if any argument contains invalid Unicode
+// use std::env::args_os  // accepts invalid Unicode. This function returns an iterator that produces OsString values instead of String values. They're more complex to work with.
+use std::process;
+
+use minigrep::Config;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();  // used to make a collection. Type annotaion is required for choosing se the collection
+    // dbg!(args);
+
+
+    // let config = Config::build(&args);
+
+    // // let config: Config = match config {
+    // //     Ok(config) => config,
+    // //     Err(erro) => {
+    // //          println!("Vai falando kk {erro}");
+    // //          process::exit(1)
+    // //     },
+    // // };
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments! {err}");
+        process::exit(1);
+    });
+
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
+}
+
+
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/gustavoski/CS/MyProjects/Command-Line-Program/minigrep/target/debug/minigrep: /home/gustavoski/CS/MyProjects/Command-Line-Program/minigrep/src/main.rs
